f479ba87f8b38923d8958975687e045074881272
Dirk Sigurdson <dirk@privacylabs.io>
2016-06-17 17:03:33 +0000

    updates to support specifying tag

--- stage2.5/00-oasis/02-run.sh
+++ stage2.5/00-oasis/02-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 on_chroot sh -e - <<EOF
+export DEBIAN_FRONTEND=noninteractive
 pip install markupsafe
 pip install cryptography --upgrade
 pip install boto
@@ -9,11 +10,12 @@ if [ ! -d oasis ]; then
 #git clone ssh://dirk@shockley.trustedether.com/git/oasis --recursive
 git clone https://github.com/privacylabs/oasis --recursive
 cd oasis && git checkout -b dev origin/dev && cd ..
+cd oasis && git submodule update --recursive && cd ..
 fi
 
 if [ ! -d /run/shm ]; then
 mkdir /run/shm
-mount /run/shm
 fi
-cd oasis && ansible-playbook -i inventory image.yml
+mount /run/shm
+cd oasis && ansible-playbook -i inventory image.yml --tags "packages"
 EOF
