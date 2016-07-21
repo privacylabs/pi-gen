2a1476ad19abf6a568a2fe9a8ed7e9f3d29c6ff5
Dirk Sigurdson <dirk@privacylabs.io>
2016-07-21 14:53:55 -0700

    adding a separate data partition that get's auto expanded at first boot

--- export-image/prerun.sh
+++ export-image/prerun.sh
@@ -11,7 +11,7 @@ mkdir -p ${ROOTFS_DIR}
 BOOT_SIZE=$(du -sh ${EXPORT_ROOTFS_DIR}/boot -B M | cut -f 1 | tr -d M)
 TOTAL_SIZE=$(du -sh ${EXPORT_ROOTFS_DIR} -B M | cut -f 1 | tr -d M)
 
-IMG_SIZE=$(expr $BOOT_SIZE \* 2 \+ $TOTAL_SIZE \+ 512)M
+IMG_SIZE=$(expr $BOOT_SIZE \* 2 \+ $TOTAL_SIZE \* 2 \+ 80)M
 
 fallocate -l ${IMG_SIZE} ${IMG_FILE}
 fdisk ${IMG_FILE} > /dev/null 2>&1 <<EOF
@@ -20,10 +20,17 @@ n
 
 
 8192
-+`expr $BOOT_SIZE \* 3`M
++`expr $BOOT_SIZE \* 2`M
 p
 t
 c
+n
+
+
+8192
+
++`expr $TOTAL_SIZE \* 2`M
+p
 n
 
 
@@ -37,12 +44,16 @@ EOF
 LOOP_DEV=`kpartx -asv ${IMG_FILE} | grep -E -o -m1 'loop[[:digit:]]+' | head -n 1`
 BOOT_DEV=/dev/mapper/${LOOP_DEV}p1
 ROOT_DEV=/dev/mapper/${LOOP_DEV}p2
+DATA_DEV=/dev/mapper/${LOOP_DEV}p3
 
 mkdosfs -n boot -S 512 -s 16 -v $BOOT_DEV > /dev/null
 mkfs.ext4 -O ^huge_file $ROOT_DEV > /dev/null
+mkfs.ext4 -O ^huge_file $DATA_DEV > /dev/null
 
 mount -v $ROOT_DEV ${ROOTFS_DIR} -t ext4
 mkdir -p ${ROOTFS_DIR}/boot
 mount -v $BOOT_DEV ${ROOTFS_DIR}/boot -t vfat
+mkdir -p ${ROOTFS_DIR}/data
+mount -v $DATA_DEV ${ROOTFS_DIR}/data -t ext4
 
 rsync ${EXPORT_ROOTFS_DIR}/ ${ROOTFS_DIR}/ -aHAX
--- stage2.5/00-oasis/01-run.sh
+++ stage2.5/00-oasis/01-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 install -m 644 files/hosts			${ROOTFS_DIR}/etc/
-
+install -m 755 files/resize_data_partition	${ROOTFS_DIR}/usr/local/sbin/
