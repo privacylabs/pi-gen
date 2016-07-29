771b431fd84606854b2d1f8603d9a7cbd0430162
Dirk Sigurdson <dirk@privacylabs.io>
2016-07-29 12:43:20 -0700

    added support for downloading new system images (currently hourly)

--- stage2.5/00-oasis/01-run.sh
+++ stage2.5/00-oasis/01-run.sh
@@ -2,3 +2,4 @@
 
 install -m 644 files/hosts			${ROOTFS_DIR}/etc/
 install -m 755 files/resize_data_partition	${ROOTFS_DIR}/usr/local/sbin/
+install -m 755 files/oasis-update		${ROOTFS_DIR}/etc/cron.hourly/
