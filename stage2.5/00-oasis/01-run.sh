f6c8e2eabac850ba78433ed8c87c118a6315845c
Dirk Sigurdson <dirk@privacylabs.io>
2016-08-02 14:17:34 -0700

    initramfs updates

--- stage2.5/00-oasis/01-run.sh
+++ stage2.5/00-oasis/01-run.sh
@@ -3,3 +3,6 @@
 install -m 644 files/hosts			${ROOTFS_DIR}/etc/
 install -m 755 files/resize_data_partition	${ROOTFS_DIR}/usr/local/sbin/
 install -m 755 files/oasis-update		${ROOTFS_DIR}/etc/cron.hourly/
+install -m 755 files/initramfs/oasis.script	${ROOTFS_DIR}/etc/initramfs-tools/scripts/init-top/oasis
+install -m 755 files/initramfs/oasis.hook	${ROOTFS_DIR}/etc/initramfs-tools/hooks/oasis
+install -m 755 files/initramfs.gz		${ROOTFS_DIR}/boot/initramfs.gz
